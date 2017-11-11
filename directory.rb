fb912d5f4165c713c1fd561ca48991836c89be2a
Tom <tom.j.v.brandon@gmail.com>
2017-11-11 18:00:38 +0000

    Added options to save list of students to students.csv file, and can also load previously saved students from this file.

--- directory.rb
+++ directory.rb
@@ -110,6 +110,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Loads list from students.csv"
     puts "9. Exit" #will be adding more items
 end
 
@@ -123,6 +124,8 @@ def process(selection)
             show_students
         when "3"
             save_students
+        when "4"
+            load_students
         when "9"
             exit #causes the program to terminates
         else
@@ -135,9 +138,18 @@ def save_students
     file = File.open("students.csv", "w")
     #iterate over the array of students
     @students.each do |student|
-        student_data = [student[:name], student[:cohort]]
+        student_data = [student[:name], student[:cohort], student[:hobby], student[:height]]
         csv_line = student_data.join(",")
         file.puts csv_line
+    end
+    file.close
+end
+
+def load_students
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+        name, cohort, hobby, height = line.chomp.split(',')
+        @students << {name: name, cohort: cohort.to_sym, hobby: hobby.to_sym, height: height.to_sym}
     end
     file.close
 end
